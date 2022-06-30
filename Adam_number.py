acd887717cc50cd682e37bd456b9a5316ad3a8e4
kundhanasrena2003 <97425765+kundhanasrena2003@users.noreply.github.com>
2022-06-30 15:35:40 +0530

    x=int(input()) s=x*x z=0 t=0 while(x):     d=x%10     x=x//10     z=z*10+d l=z*z while(l):     d=l%10     l=l//10     t=t*10+d if(t==s):     print("True") else:     print("False")

---
+++ Adam_number.py
@@ -0,0 +1,17 @@
+x=int(input())
+s=x*x
+z=0
+t=0
+while(x):
+    d=x%10
+    x=x//10
+    z=z*10+d
+l=z*z
+while(l):
+    d=l%10
+    l=l//10
+    t=t*10+d
+if(t==s):
+    print("True")
+else:
+    print("False")
